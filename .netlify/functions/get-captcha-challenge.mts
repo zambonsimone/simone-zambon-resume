ac216f54ee4641405580836078482813d1042c59
Simone Zambon <simone.zambon@grupposandonato.it>
2025-03-03 17:39:40 +0100

    fix challenges json for captcha

--- .netlify/functions/get-captcha-challenge.mts
+++ .netlify/functions/get-captcha-challenge.mts
@@ -1,7 +1,7 @@
 import { getCaptchaChallenge } from "../../server/captcha/get-captcha-challenge.mjs";
 
-export default async () => {
-    const response = await getCaptchaChallenge();
+export default () => {
+    const response = getCaptchaChallenge();
     return new Response(JSON.stringify(response), {
         status: response.statusCode,
         headers: {
--- server/captcha/utils.mjs
+++ server/captcha/utils.mjs
@@ -1,11 +1,8 @@
 import fs from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
 
 function getChallengeList() {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    const filePath = path.join(__dirname, "captcha-challenges.json");
+    const filePath = path.join(process.cwd(), "server/captcha/captcha-challenges.json");
     const rawData = fs.readFileSync(filePath);
     return JSON.parse(rawData).challenges;
 }
