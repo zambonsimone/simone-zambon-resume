670e2954a8b5ad5b5e0b28350a29527fcf5ce3c1
Simone Zambon <simone.zambon@grupposandonato.it>
2025-03-08 15:39:51 +0100

    skill level legenda and symbols

--- src/components/skills/skill-level-legenda/SkillLevelLegenda.tsx
+++ src/components/skills/skill-level-legenda/SkillLevelLegenda.tsx
@@ -1,28 +1,34 @@
-import React from "react";
+import { useTranslation } from "react-i18next";
 import { Accordion } from "../../common/accordion/Accordion";
+import { LEVEL_INDICATOR_LABEL } from "../../route/content/text-content/level-indicator/labels";
 import { getLevelBgColor } from "../../route/content/text-content/level-indicator/utils";
 import style from "./SkillLevelLegenda.module.scss";
 import { LEVEL_LEGENDA_LABELS } from "./labels";
 
 
 const SkillLevelLegenda: React.FC = () => {
+    const { t } = useTranslation("global");
     return (
         <Accordion
-            header={LEVEL_LEGENDA_LABELS.HEADER}
+            header={t(LEVEL_LEGENDA_LABELS.HEADER)}
             className={style.levelLegendaContainer}
             contentClassName={style.levelLegendaContent}
             content={(
                 <div>
-                    <ul>
-                        { Object.entries(LEVEL_LEGENDA_LABELS.LEVELS).map(([level, description], index) => (
-                            <li key={index}>
-                                <span className={style.level} style={{ backgroundColor: getLevelBgColor(+level) }}>
+                    <dl role="list">
+                        {Object.entries(LEVEL_LEGENDA_LABELS.LEVELS).map(([level, description], index) => (
+                            <div
+                                role="listitem"
+                                key={index}
+                                aria-label={`${t(LEVEL_INDICATOR_LABEL)} ${level}: ${t(description)}`}
+                            >
+                                <dt className={style.level} style={{ backgroundColor: getLevelBgColor(+level) }}>
                                     {level}
-                                </span>
-                                <span className={style.description}>{description}</span>
-                            </li>
+                                </dt>
+                                <dd className={style.description}>{t(description)}</dd>
+                            </div>
                         ))}
-                    </ul>
+                    </dl>
                 </div>
             )}
         />
