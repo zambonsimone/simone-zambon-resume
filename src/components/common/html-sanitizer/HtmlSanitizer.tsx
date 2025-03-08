b6637cbc7167646228dcc9306e2743c8712e51b7
Simone Zambon <simone.zambon@grupposandonato.it>
2025-03-08 15:37:47 +0100

    base components:
    headerbar, html sanitizer, navbar, searchbar, subnavbar, sidebar

--- src/components/common/html-sanitizer/HtmlSanitizer.tsx
+++ src/components/common/html-sanitizer/HtmlSanitizer.tsx
@@ -1,21 +1,36 @@
+import { HTMLAttributes, useMemo } from "react";
 import sanitizeHtml from "sanitize-html";
 
-interface IHtmlSanitizerProps {
+interface IHtmlSanitizerProps extends HTMLAttributes<HTMLElement> {
     htmlString: string;
     className?: string;
+    element?: keyof HTMLElementTagNameMap;
 }
 
-export const HtmlSanitizer: React.FC<IHtmlSanitizerProps> = ({ 
+export const HtmlSanitizer: React.FC<IHtmlSanitizerProps> = ({
     htmlString,
     className,
+    element,
+    ...elementAttributes
 }) => {
     const sanitizedHTML = sanitizeHtml(htmlString, {
-        allowedTags: ["a","br"],
-        allowedAttributes: { 
-            a: ["href"] 
+        allowedTags: ["a", "br", "code", "strong", "svg", "path"],
+        allowedAttributes: {
+            a: ["href", "target", "aria-label"],
+            svg: ["xmlns","width","height","viewBox"],
+            path: ["d","stroke","fill"]
         }
     })
-    return (
-        <span className={className} dangerouslySetInnerHTML={{ __html: sanitizedHTML }}/>
-    )
+
+    const props: HTMLAttributes<HTMLElement> = useMemo(() => ({
+        ...elementAttributes,
+        className,
+        dangerouslySetInnerHTML: { __html: sanitizedHTML },
+    }), [className, elementAttributes, sanitizedHTML]);
+
+    switch (element) {
+        case "p": return <p {...props} />
+        case "span": return <span {...props} />
+        default: return <div {...props} />
+    }
 }
