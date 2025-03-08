ec1792dc016cd12fcbfaf5607bac8bf665bfb1f7
Simone Zambon <simone.zambon@grupposandonato.it>
2025-03-08 15:18:24 +0100

    base index.html, css data and deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { App } from './App';
 
-ReactDOM.createRoot( 
-  document.querySelector('#root')
-).render(<App />)
+function start() {
+  ReactDOM.createRoot(
+    document.querySelector('#root')
+  ).render(<App />)
+}
+
+start();
+
+
