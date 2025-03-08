55652f4daa7b71e6ad3e7406892738e2135d43d4
Simone Zambon <simone.zambon@grupposandonato.it>
2025-03-08 15:40:19 +0100

    app main content components

--- src/components/route/content/text-content/Paragraphs.tsx
+++ src/components/route/content/text-content/Paragraphs.tsx
@@ -1,18 +1,22 @@
+import { TFunction } from "i18next";
 import { HtmlSanitizer } from "../../../common/html-sanitizer/HtmlSanitizer";
 import style from "./TextContent.module.scss";
 
 interface IParagraphsProps {
     paragraphs: string[] | undefined;
+    tFunction: TFunction
 }
 
 export const Paragraphs: React.FC<IParagraphsProps> = ({
-    paragraphs
+    paragraphs,
+    tFunction: t
 }) => {
     return paragraphs?.map((parag, index) => (
-        <HtmlSanitizer 
-            className={style.sectionParagraph} 
-            key={index} 
-            htmlString={parag}
-        />   
+        <HtmlSanitizer
+            className={style.sectionParagraph}
+            key={index}
+            htmlString={t(parag)}
+            element={"p"}
+        />
     ))
 }
--- src/components/route/content/text-content/TextContent.tsx
+++ src/components/route/content/text-content/TextContent.tsx
@@ -1,30 +1,36 @@
+import { TFunction } from "i18next";
 import { IRouteContentOrganization } from "../../../../types";
 import { Details } from "./Details";
 import { Paragraphs } from "./Paragraphs";
+import { SubsectionTitle } from "./SubsectionTitle";
 import style from "./TextContent.module.scss";
 import { Title } from "./Title";
 
 interface ITextContentProps {
     sections: IRouteContentOrganization["SECTIONS"];
+    tFunction: TFunction
 }
 
-export const TextContent: React.FC<ITextContentProps> = ({ sections }) => {
+export const TextContent: React.FC<ITextContentProps> = ({
+    sections,
+    tFunction: t
+}) => {
     return sections.map((section, index) => (
         <div key={index} className={style.sectionContainer}>
             <div className={style.header}>
-                <Title title={section.TITLE}/>
-                <Details details={section.DETAILS}/>
+                <Title title={t(section.TITLE)} />
+                <Details details={section.DETAILS} tFunction={t} />
             </div>
-            <Paragraphs paragraphs={section.PARAGRAPHS}/>
-            { section.SUB_SECTIONS?.map((subs, index) => (
+            <Paragraphs paragraphs={section.PARAGRAPHS} tFunction={t} />
+            {section.SUB_SECTIONS?.map((subs, index) => (
                 <div key={index} className={style.subSectionContainer}>
                     <div className={style.header}>
-                        <Title title={subs.TITLE}/>
-                        <Details details={subs.DETAILS}/>
+                        <SubsectionTitle title={t(subs.TITLE)} />
+                        <Details details={subs.DETAILS} tFunction={t} />
                     </div>
-                    <Paragraphs paragraphs={subs.PARAGRAPHS}/>
+                    <Paragraphs paragraphs={subs.PARAGRAPHS} tFunction={t} />
                 </div>
             ))}
-        </div>  
+        </div>
     ))
 }
